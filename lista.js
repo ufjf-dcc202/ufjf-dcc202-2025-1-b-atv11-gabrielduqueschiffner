1857356bd65a88c130fef6ccbbf1855be569b3e1
Gabriel Duque Schiffner Oliveira <gabrielduque.schiffner@estudante.ufjf.br>
2025-07-19 12:31:45 -0300

    add funcao atualiza lista e o lista.js

---
+++ lista.js
@@ -0,0 +1,11 @@
+//lista.js
+
+const lista = ["Um", "Dois", "Três"];
+
+export function getLista(){
+    return structuredClone(lista);
+}
+
+export function limpaLista(){
+    lista.splice(0);
+}
--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 //main.js
 
+import { getLista } from "./lista";
+
 const o1Itens = document.querySelector ("#itens");
-const pEntrada = document.querySelector("#adicionar");
+const pEntrada = document.querySelector("#entrada");
+const btnAdicionar = document.querySelector("adicionar")
 const btnLimpar = document.querySelector("#limpar");
+
+atualizarLista();
+
+function atualizarLista(){
+    o1Itens.innerHTML = "";
+    let lista = getLista();
+    for(let i = 0; i < lista.length; i++){
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        o1Itens.appendChild(li);
+    }
+}
